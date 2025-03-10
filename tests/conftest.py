73b61447420285a45b8dcfc596668fb2f38a4ba2
Max_Razzhivin <max.nvo06@gmail.com>
2025-03-10 10:05:39 +0300

    delete --headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,6 @@ def browser_configuration():
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
-    # driver_options.add_argument('--headless')
     browser.config.driver_options = driver_options
 
     yield
