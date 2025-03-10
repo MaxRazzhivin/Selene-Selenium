d93c449ec7b302fea2a85aae468d97cd79a3df4a
Max_Razzhivin <max.nvo06@gmail.com>
2025-03-10 19:43:51 +0300

    fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ def browser_configuration():
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
+    driver_options.add_argument('--headless')
     browser.config.driver_options = driver_options
 
     yield
